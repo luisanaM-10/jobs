311cf2ace1b524a217513474f8131a62754a82b8
luisana muñoz <luisanamunoz879@gmail.com>
2023-02-15 11:05:05 -0300

    id indentificados

--- app.js
+++ app.js
@@ -3,13 +3,7 @@
 document.getElementById("btnBurge").addEventListener(`click`, (e) => document.getElementById(`div-burger`).classList.toggle(`oculto`))
 
 
-// función de ocultar vista principal y mostrar formulario 
-const isHiddenForm = (`click`, (e) =>{
-    document.getElementById("formulario").classList.remove("oculto")
-    document.getElementById("vista-principal").classList.add("oculto")
-})
-document.getElementById("btn-create").addEventListener("click", isHiddenForm)
-document.getElementById("btn-create2").addEventListener("click", isHiddenForm)
+
 
 
 
